29dde2c4b5b332eada15818561bc0f0ff13aadb2
izacgaldino <izac.galdino@bemobi.com>
2023-03-08 15:45:40 -0300

    Move change error

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"net/http"
-	"strings"
 
-	goerrors "github.com/go-errors/errors"
 	"github.com/gofiber/fiber/v2"
-	arrayfuncs "github.com/izacgaldino23/array-funcs"
 	"github.com/izacgaldino23/products-api/config"
 	"github.com/izacgaldino23/products-api/handler/product"
-	"github.com/izacgaldino23/products-api/utils"
+	"github.com/izacgaldino23/products-api/oops"
 )
 
 func init() {
@@ -29,7 +24,7 @@ func init() {
 func main() {
 	app := fiber.New(fiber.Config{
 		// Override default error handler
-		ErrorHandler: handleError,
+		ErrorHandler: oops.HandleError,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -52,53 +47,4 @@ func handle(app *fiber.App) {
 	v1 := api.Group("/v1")
 
 	product.Router(v1.Group("/product"))
-}
-
-func handleError(ctx *fiber.Ctx, err error) error {
-	code := fiber.StatusInternalServerError
-
-	// Retrieve the custom status code if it's a *fiber.Error
-	var e *fiber.Error
-	if errors.As(err, &e) {
-		code = e.Code
-	}
-
-	if err != nil {
-		errorMessage := utils.T{
-			"error": err.Error(),
-		}
-
-		if code != http.StatusUnprocessableEntity {
-			stack := err.(*goerrors.Error).ErrorStack()
-
-			stackColor := arrayfuncs.AnyToArrayKind(strings.Split(stack, "\n"))
-
-			stackColor.ForEach(func(line string, i int, a *[]string) {
-				if strings.Contains(line, " (") {
-					sentences := strings.Split(line, " ")
-
-					sentences[0] = utils.Colorize(utils.Red, sentences[0])
-					sentences[1] = utils.Colorize(utils.Gray, sentences[1])
-
-					print(sentences[0], " ")
-					println(sentences[1])
-
-					// line = strings.Join(sentences, " ")
-				} else if v := strings.TrimSpace(line); v != "" {
-					println(utils.Colorize(utils.Green, "   ->"), v)
-				}
-			})
-
-			stack = strings.ReplaceAll(stack, "\t", "    ")
-			stack = strings.ReplaceAll(stack, "\n", "\n    ")
-
-			errorMessage["stack"] = strings.Split(stack, "\n")
-		}
-
-		// In case the SendFile fails
-		return ctx.Status(code).JSON(errorMessage)
-	}
-
-	// Return from handler
-	return nil
 }
--- oops/oops.go
+++ oops/oops.go
@@ -1,6 +1,14 @@
 package oops
 
-import "github.com/go-errors/errors"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/go-errors/errors"
+	fiber "github.com/gofiber/fiber/v2"
+	arrayfuncs "github.com/izacgaldino23/array-funcs"
+	"github.com/izacgaldino23/products-api/utils"
+)
 
 func Wrap(err error, msg string) error {
 	return errors.WrapPrefix(err, msg, 0)
@@ -8,4 +16,53 @@ func Wrap(err error, msg string) error {
 
 func Err(err error) error {
 	return errors.Wrap(err, 1)
+}
+
+func HandleError(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+
+	// Retrieve the custom status code if it's a *fiber.Error
+	var e *fiber.Error
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+
+	if err != nil {
+		errorMessage := utils.T{
+			"error": err.Error(),
+		}
+
+		if code != http.StatusUnprocessableEntity {
+			stack := err.(*errors.Error).ErrorStack()
+
+			stackColor := arrayfuncs.AnyToArrayKind(strings.Split(stack, "\n"))
+
+			stackColor.ForEach(func(line string, i int, a *[]string) {
+				if strings.Contains(line, " (") {
+					sentences := strings.Split(line, " ")
+
+					sentences[0] = utils.Colorize(utils.Red, sentences[0])
+					sentences[1] = utils.Colorize(utils.Gray, sentences[1])
+
+					print(sentences[0], " ")
+					println(sentences[1])
+
+					// line = strings.Join(sentences, " ")
+				} else if v := strings.TrimSpace(line); v != "" {
+					println(utils.Colorize(utils.Green, "   ->"), v)
+				}
+			})
+
+			stack = strings.ReplaceAll(stack, "\t", "    ")
+			stack = strings.ReplaceAll(stack, "\n", "\n    ")
+
+			errorMessage["stack"] = strings.Split(stack, "\n")
+		}
+
+		// In case the SendFile fails
+		return ctx.Status(code).JSON(errorMessage)
+	}
+
+	// Return from handler
+	return nil
 }
